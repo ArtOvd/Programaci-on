b71ed590ec830e4f4bff319a5d19fc8241c2000e
artem <artovd@alu.edu.gva.es>
2026-01-03 13:18:24 +0100

    Juego ConectaCuatro acabado. Faltan los comentarios (opcional)

--- UD4/RetoUD4/ConectaCuatro.java
+++ UD4/RetoUD4/ConectaCuatro.java
@@ -4,23 +4,49 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConectaCuatro {
-    static char[][] tablero = new  char[6][7];
+    static char[][] tablero = new char[6][7];
     static int[] columnas = {1, 2, 3, 4, 5, 6, 7};
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         tablero = llenarTablero();
-        imprimirTablero();
-        int entrada = jugadaUsuario();
-        int validar = buscarFilaLibre(entrada);
-        System.out.println("Columna " + (entrada + 1) + " vacía: " + validar);
+        char jugadorActual = 'X';
+        boolean fin = false;
+        int contador = 0;
+
+        while (!fin) {
+            imprimirTablero();
+            System.out.println("Turno de: " + jugadorActual);
+            int jugada = jugadaUsuario();
+            if (colocarFicha(jugada, jugadorActual)) {
+                contador++;
+                if (comprobarVictoria(jugadorActual)) {
+                    imprimirTablero();
+                    System.out.println("EL JUGADOR " + jugadorActual + " HA GANADO!");
+                    fin = true;
+                } else if (contador == 42) {
+                    imprimirTablero();
+                    System.out.println("EMPATE! No quedan más movimientos.");
+                    fin = true;
+                } else {
+                    if (jugadorActual == 'X') {
+                        jugadorActual = 'O';
+                    } else {
+                        jugadorActual = 'X';
+                    }
+                }
+            }
+        }
     }
 
-    static char[][] llenarTablero () {
+    static char[][] llenarTablero() {
         for (int i = 0; i < tablero.length; i++) {
             Arrays.fill(tablero[i], '·');
         }
         return tablero;
     }
-    static void imprimirTablero () {
+
+    static void imprimirTablero() {
         System.out.println("=========================");
         System.out.println("||       TABLERO       ||");
         System.out.println("=========================");
@@ -32,7 +58,7 @@ public class ConectaCuatro {
             }
         }
         System.out.println();
-        for  (int i = 0; i < tablero.length; i++) {
+        for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
                 if (j == 6) {
                     System.out.print(tablero[i][j]);
@@ -46,7 +72,6 @@ public class ConectaCuatro {
     }
 
     static int jugadaUsuario() {
-        Scanner sc = new Scanner(System.in);
         int entrada;
         while (true) {
             System.out.print("Columna a elegir (1-7): ");
@@ -58,12 +83,89 @@ public class ConectaCuatro {
         }
     }
 
-    static int buscarFilaLibre(int columna) {
+    static boolean comprobarFila(int columna) {
         for (int i = tablero.length - 1; i >= 0; i--) {
             if (tablero[i][columna] == '·') {
-                return i;
+                return true;
             }
         }
-        return -1;
+        return false;
+    }
+
+    static boolean colocarFicha(int columna, char jugador) {
+        if (!comprobarFila(columna)) {
+            System.out.println("Esta columna está llena. Elige otra.");
+            return false;
+        }
+        for (int i = tablero.length - 1; i >= 0; i--) {
+            if (tablero[i][columna] == '·') {
+                tablero[i][columna] = jugador;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean comprobarVictoria(char jugador) {
+        return comprobarHorizontal(jugador) ||
+                comprobarVertical(jugador) ||
+                comprobarDiagonal1(jugador) ||
+                comprobarDiagonal2(jugador);
+    }
+
+    static boolean comprobarHorizontal(char jugador) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tablero[i][j] == jugador &&
+                        tablero[i][j + 1] == jugador &&
+                        tablero[i][j + 2] == jugador &&
+                        tablero[i][j + 3] == jugador) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean comprobarVertical(char jugador) {
+        for (int j = 0; j < 7; j++) {
+            for (int i = 0; i < 3; i++) {
+                if (tablero[i][j] == jugador &&
+                        tablero[i + 1][j] == jugador &&
+                        tablero[i + 2][j] == jugador &&
+                        tablero[i + 3][j] == jugador) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean comprobarDiagonal1(char jugador) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tablero[i][j] == jugador &&
+                        tablero[i + 1][j + 1] == jugador &&
+                        tablero[i + 2][j + 2] == jugador &&
+                        tablero[i + 3][j + 3] == jugador) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean comprobarDiagonal2(char jugador) {
+        for (int i = 3; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tablero[i][j] == jugador &&
+                        tablero[i - 1][j + 1] == jugador &&
+                        tablero[i - 2][j + 2] == jugador &&
+                        tablero[i - 3][j + 3] == jugador) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
