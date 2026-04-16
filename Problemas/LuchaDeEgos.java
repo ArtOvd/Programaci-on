b669e58fd3e99af88add27453e544d3d40691194
Artem Ovdiienko <artovd.alu.edu.gva.es>
2026-04-16 12:16:47 +0200

    problemas para practicar

--- Problemas/LuchaDeEgos.java
+++ Problemas/LuchaDeEgos.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class LuchaDeEgos {
 
@@ -8,45 +9,57 @@ public class LuchaDeEgos {
         do {
             int count = 0;
             num = sc.nextInt();
-            int[] fuerzas = new int[num];
-            String[] deseados = new String[num];
-            String pareja = "";
 
-            for (int i = 0; i< fuerzas.length; i++){
-                fuerzas[i] = sc.nextInt();
+            ArrayList<Integer> fuerzas = new ArrayList<>();
+            ArrayList<String> deseados = new ArrayList<>();
+
+            for (int i = 0; i < num; i++){
+                fuerzas.add(sc.nextInt());
             }
-            for (int i = 0; i < deseados.length; i++){
-                    deseados[i] = sc.next();
+            for (int i = 0; i < num; i++){
+                deseados.add(sc.next());
             }
 
-            for (int i = 0; i < fuerzas.length; i++){
-                for  (int j = i + 1; j < deseados.length - 1; j++){
-                    if (deseados[i].startsWith("=")){
-                        if (deseados[i].charAt(1) == fuerzas[j]){
-                            pareja = fuerzas[i] + " " + fuerzas[j];
-                            count++;
-                        }
-                    } else if (deseados[i].startsWith(">")){
-                        if (deseados[i].charAt(1) < fuerzas[j]){
-                            pareja = fuerzas[i] + " " + fuerzas[j];
-                            count++;
-                        }
-                    } else if (deseados[i].startsWith("<")){
-                        if (deseados[i].charAt(1) > fuerzas[j]){
-                            pareja = fuerzas[i] + " " +  fuerzas[j];
+            for (int i = 0; i < fuerzas.size(); i++){
+                for (int j = i + 1; j < deseados.size() ; j++) {
+                    if (fuerzas.get(i) != 0 && deseados.get(j) != null) {
+                        if (deseados.get(i).startsWith("=")) {
+                            if (Integer.parseInt(String.valueOf(deseados.get(i).charAt(1))) == fuerzas.get(j) && Integer.parseInt(String.valueOf(deseados.get(j).charAt(1))) == fuerzas.get(i)) {
+                                System.out.println((i + 1) + " " + (j + 1));
+                                count++;
+                                fuerzas.set(i, 0);
+                                deseados.set(i, null);
+                                fuerzas.set(j, 0);
+                                deseados.set(j, null);
+                            }
+                        } else if (deseados.get(i).startsWith(">")) {
+                            if (Integer.parseInt(String.valueOf(deseados.get(i).charAt(1))) < fuerzas.get(j) && Integer.parseInt(String.valueOf(deseados.get(j).charAt(1))) > fuerzas.get(i)) {
+                                System.out.println((i + 1) + " " + (j + 1));
+                                count++;
+                                fuerzas.set(i, 0);
+                                deseados.set(i, null);
+                                fuerzas.set(j, 0);
+                                deseados.set(j, null);
+                            }
+                        } else if (deseados.get(i).startsWith("<")) {
+                            if (Integer.parseInt(String.valueOf(deseados.get(i).charAt(1))) > fuerzas.get(j) && Integer.parseInt(String.valueOf(deseados.get(j).charAt(1))) < fuerzas.get(i)) {
+                                System.out.println((i + 1) + " " + (j + 1));
+                                count++;
+                                fuerzas.set(i, 0);
+                                deseados.set(i, null);
+                                fuerzas.set(j, 0);
+                                deseados.set(j, null);
+                            }
                         }
                     }
-                    if (count > 0) {
-                        System.out.println(pareja);
-                    } else {
-                        System.out.println("NO HAY");
-                    }
-                    System.out.println("---");
                 }
             }
 
+            if (count < 1) {
+                System.out.println("NO HAY");
+            }
+            System.out.println("---");
+
         } while (num > 0 && num <= 100);
-
     }
-
 }
