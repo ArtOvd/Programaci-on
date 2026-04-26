02641bd0c071d68c41d3004fbfd40dc0117eb4e4
artem <artovd@alu.edu.gva.es>
2026-04-26 18:49:12 +0200

    Queue y HashSet resueltos

--- UD7/PRFIFO.java
+++ UD7/PRFIFO.java
@@ -1,7 +1,81 @@
+import java.util.*;
+
 public class PRFIFO {
 
     public static void main(String[] args) {
+        // EJ 1:
+//        Queue<String> cola = new LinkedList<>(List.of("Artem", "Pablo", "Nela", "Samuel", "Alejandro"));
+//        cola.poll();
+//        cola.poll();
+//        System.out.println(cola);
+//            System.out.println("Siguiente para atender: " + cola.peek());
+//        cola.add("Lio");
+//        System.out.println(cola);
 
+        //EJ 2:
+//        class Documento {
+//            private String nombre;
+//            private int cantidadPaginas;
+//            Documento(String nombre, int cantidadPaginas) {
+//                this.nombre = nombre;
+//                this.cantidadPaginas = cantidadPaginas;
+//            }
+//            public String getNombre() {
+//                return nombre;
+//            }
+//            public int getCantidadPaginas() {
+//                return cantidadPaginas;
+//            }
+//        }
+//
+//        Documento documento1 = new Documento("LM", 1);
+//        Documento documento2 = new Documento("ED", 2);
+//        Documento documento3 = new Documento("PR", 5);
+//        Queue<Documento> documentos = new LinkedList<>(List.of(documento1, documento2, documento3));
+//        while (!documentos.isEmpty()) {
+//            Documento documento = documentos.poll();
+//            System.out.println("Se imprime: " +  documento.getNombre());
+//            System.out.println("Tiempo a esperar: " + documento.getCantidadPaginas());
+//            try {
+//                Thread.sleep(documento.getCantidadPaginas() * 1000L);
+//            }  catch (InterruptedException e) {
+//                System.out.println("Error.");
+//            }
+//        }
+//        System.out.println("Se imprimió todo.");
+        // EJ 3:
+        class Proceso {
+            private String nom;
+            private int time;
+
+            Proceso(String nom, int time) {
+                this.nom = nom;
+                this.time = time;
+            }
+
+            public String getNom() {
+                return nom;
+            }
+            public int getTime() {
+                return time;
+            }
+            public void setTime(int time) {
+                this.time = time;
+            }
+        }
+        Proceso p1 =  new Proceso("Twitch", 10);
+        Proceso p2 = new Proceso("YouTube", 5);
+        Proceso p3 = new Proceso("ItelijIDEA", 7);
+        Proceso p4 = new Proceso("Spotify", 8);
+        Queue<Proceso> queue = new LinkedList<>(List.of(p1, p2, p3, p4));
+        while (!queue.isEmpty()) {
+            Proceso p = queue.poll();
+            System.out.println("Proceso actual: " + p.getNom());
+            System.out.println("Tiempò de restante: " + p.getTime());
+            if (p.getTime() - 2 > 0) {
+                p.setTime(p.getTime() - 2);
+                queue.add(p);
+            }
+        }
     }
-
 }
---
+++ UD7/PRHashSet.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class PRHashSet {
+
+    public static void main(String[] args) {
+        // EJ 1:
+//        Set<String> set = new HashSet<String>();
+//        Scanner sc = new Scanner(System.in);
+//        while (true) {
+//            String invitado = sc.nextLine();
+//            if (invitado.equals("fin")) {
+//                break;
+//            }
+//            set.add(invitado);
+//        }
+//        System.out.println(set);
+
+        // EJ 2:
+//        ArrayList<Integer> lista = new ArrayList<>();
+//        for  (int i = 0; i < 20; i++) {
+//            lista.add((int) (Math.random() * 100) + 1);
+//        }
+//        System.out.println(lista);
+//        Set<Integer> set = new HashSet<>(lista);
+//        System.out.println(set);
+//        System.out.println("Longitud: " + set.size());
+
+        // EJ 3:
+        Scanner sc = new Scanner(System.in);
+        String cadena =  sc.nextLine().toLowerCase();
+        Set<String> palabras = new HashSet<>(List.of(cadena.split(" ")));
+        System.out.println(palabras);
+    }
+
+}
